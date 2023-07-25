402cea3f2d3ff9b355e0ebad600398c8a8d6c5c1
gohshiyong12092 <shiyong@iastate.edu>
2023-07-25 04:03:48 -0500

    add c code

--- modifyGcode.c
+++ modifyGcode.c
@@ -1,8 +1,42 @@
 #include<stdio.h>
-
+#include<stdbool.h>
 #define MAX_LINE_LENGTH 1024
 
+void modifyLayer(char *str, int num) {
+    char *layerPtr = strstr(str, ";LAYER:"); // Find the occurrence of ";LAYER:"
+    if (layerPtr != NULL) {
+        int layerNumber = atoi(layerPtr + 7);
+        layerNumber -= num;
+        //update the string 
+        snprintf(layerPtr + 7, 10, "%d\n",layerNumber);
+    }
+}
+
+void modifyZValue(char *line, double newValue) {
+    // Find the substring containing "Z"
+    char *zPtr = strstr(line, "Z");
+    if (zPtr == NULL) {
+        // The substring "Z" was not found in the line
+        return;
+    }
+
+    // Extract the numeric value after "Z"
+    double currentValue;
+    if (sscanf(zPtr + 1, "%lf", &currentValue) != 1) {
+        // Failed to extract the current numeric value after "Z"
+        return;
+    }
+
+    // Modify the value
+    currentValue -= newValue;
+
+    // Update the value in the line
+    sprintf(zPtr + 1, "%.2f", currentValue);
+}
+
 int main(){
+    void modifyLayer(char *str, int num);
+    void modifyZValue(char *line, double newValue);
     FILE* inputFile;
     FILE* outputFile;
     char inputFileName[100];
@@ -11,7 +45,7 @@ int main(){
     int layerToRemove;
     int numberOfLayers;
     char currentLayer[10];
-    // int currentLayer;
+    int current;
 
     //prompt user to enter the input file name 
     printf("Enter the input G-code file name: ");
@@ -32,17 +66,45 @@ int main(){
         perror("Error opening the input file");
         return 1;
     }
-
+    //open the file to write
+    outputFile = fopen(outputFileName,"w");
+    if(outputFile == NULL){
+        perror("Error opening the output file");
+        return 1;
+    }
+    bool Flag_Target = 0;
+    bool Flag_Found = 0;
+    float ModifyVal = 0.15 * numberOfLayers;
+    int count_remove = 0;
     //find the line to remove 
     while(fgets(line,sizeof(line), inputFile) != NULL){
         if(strstr(line,";LAYER:") != NULL){
             sscanf(line,";LAYER:%s",&currentLayer);
-            printf("%s\n",currentLayer);
+            current = atoi(currentLayer);
         }
 
-        if(currentLayer == layerToRemove){
-            printf("Found Line: %d\n",line);
-            break;
+        if(current == layerToRemove){
+            Flag_Target = 1;
+            printf("Found Line: %s\n",line);
+            current = 0;
+        }
+        if(Flag_Target == 0){
+            if(Flag_Found == 1 && (strstr(line,";LAYER:") != NULL)){
+                modifyLayer(line,numberOfLayers);
+                modifyZValue(line,ModifyVal);
+            }
+            fputs(line, outputFile);
+            
+            
+        }
+        else{
+            if(strstr(line,";MESH:NONMESH") != NULL && (count_remove < numberOfLayers)){
+               
+                fputs(line, outputFile);
+                Flag_Target = 0;
+                Flag_Found = 1;
+                count_remove += 1;
+            }
         }
     }
 
